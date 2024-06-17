d266970f15eeabc2b6d5897a6a4cae450d3b30ee
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-06-17 18:48:37 +0530

    Time: 0 ms (100%), Space: 7.5 MB (52.04%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int reverse(int number) {
         double reverseNumber = 0;
-        while(abs(number) > 0){
+        while(number){
         int lastDigit = number%10;
         reverseNumber = reverseNumber*10 + lastDigit;
         number = number/10;
         }
        if(reverseNumber < INT_MIN || reverseNumber > INT_MAX){
-    return 0;}
+    return 0;
+       }
         return reverseNumber;
     }
 };
