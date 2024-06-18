66a3fd66ae14b9110d60a03c483521c2d9e9b008
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-06-18 07:12:40 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/All divisors of a Number/all-divisors-of-a-number.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void print_divisors(int n) {
+        vector<int> ls;
+        for(int i=1 ; i*i <= n ; i++){
+            if(n%i == 0){
+                ls.push_back(i);
+                if(n/i !=i){
+                    ls.push_back(n/i);
+                }
+            }
+        }
+        
+    sort(ls.begin(), ls.end());
+    for(int n: ls) cout << n << " ";
+      
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        ob.print_divisors(n);
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
