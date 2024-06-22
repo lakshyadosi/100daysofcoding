91e6ba7959d3a0791fc3db246d53c88290470ee6
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-06-22 15:10:36 +0530

    Update number-hashing-using-map.cpp

--- Hashing/number-hashing-using-map.cpp
+++ Hashing/number-hashing-using-map.cpp
@@ -1,1 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+/*
+if there are lower case letters only then size is 26
+if there are upper case letters also then size is 256 and then we directly create the hadh map like
+
+for(int i=0;i<s.size();i++){
+  hash[s[i]] += 1;
+}
+
+ cout <<hash[c] << " ";
+*/  
+
+int main(){
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i=0 ;i<n ;i++){
+    cin >> arr[i]; 
+  }
+
+  //Pre computation 
+  map<int, int> mpp;
+  for(int i=0;i<n;i++){
+    mpp[arr[i]] = mpp[arr[i]] + 1;
+  }
+  int q; 
+  cin>> q;
+  while(q--){
+    int number;
+    cin >> number;
+    
+      cout <<mpp[number] << " ";
+    
+  }
+}
