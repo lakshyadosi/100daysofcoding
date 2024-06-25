d9d860c857d3eb912855958a3f11363eb15bd4e1
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-06-25 07:05:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -21,41 +21,42 @@ class Solution
     public:
     void merge(int arr[], int low, int mid, int high)
     {
-        int left = low;
-        int right = mid +1;
-        vector<int> temp;
-        while(left <=mid && right <=high){
-            if(arr[left] <= arr[right]){
-                temp.push_back(arr[left]);
-                left++;
-            }
-            else{
-                temp.push_back(arr[right]);
-                right++;
-            }
-        }
-        while(left<=mid){
-            temp.push_back(arr[left]);
-            left++;
-        }
+       int left = low;
+       int right = mid +1;
+       vector<int> ls;
+       while(left <=mid && right <=high){
+           if(arr[left] < arr[right]){
+               ls.push_back(arr[left]);
+               left++;
+           }
+           else{
+               ls.push_back(arr[right]);
+               right++;
+           }
+       }
+       while(left<=mid){
+           ls.push_back(arr[left]);
+           left++;
+       }
         while(right<=high){
-            temp.push_back(arr[right]);
-            right++;
-        }
-        for(int i=low;i<=high;i++){
-          arr[i] = temp[i - low];
-        }
+           ls.push_back(arr[right]);
+           right++;
+       }
+       for(int i=low; i<=high;i++){
+           arr[i] = ls[i-low];
+       }
     }
     public:
     void mergeSort(int arr[], int low, int high)
     {   
-        if(low >= high) return;
-        int mid = low + (high-low)/2;
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid+1, high);
-        merge(arr, low, mid, high);
+       if(low >= high) return;
+       int mid = low + (high-low)/2;
+       mergeSort(arr, low, mid);
+       mergeSort(arr,mid+1, high);
+       merge(arr, low, mid, high);
     }
 };
+
 
 //{ Driver Code Starts.
 
