2bdf826dce986f52f75752f3b37cf4d049f82bfe
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-07-02 23:08:51 +0530

    Time: 211 ms (30.83%), Space: 75.5 MB (11.4%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,28 +1,53 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) {
+   int longestConsecutive(vector<int>& nums) {
         if(nums.size() == 0){
             return 0;
         }
-        sort(nums.begin(), nums.end());
-        int prev = nums[0];
-        int maxLength = 1;
-        int length = 1;
-    
-        for(int i =1 ; i< nums.size(); i++){
-            if(prev == nums[i]){
-                continue;
+
+        set<int> mySet;
+        for(int i=0;i< nums.size(); i++){
+            mySet.insert(nums[i]);
+        }
+        int maxLength = 0;
+
+        for(auto ele: mySet){
+            int prev = ele - 1;
+            if(mySet.find(prev) == mySet.end()){
+                int len = 1;
+                int nextEle = ele + 1;
+                while(mySet.find(nextEle) != mySet.end()){
+                    len++;
+                    nextEle ++;
+                }
+                maxLength = max(maxLength, len);
             }
-           if(prev + 1 == nums[i]){
-             prev++;
-             length ++;
-            }
-            else {
-              prev = nums[i];
-              length = 1;
-             }
-         maxLength = max(length, maxLength);
         }
         return maxLength;
     }
+    // int longestConsecutive(vector<int>& nums) {
+    //     if(nums.size() == 0){
+    //         return 0;
+    //     }
+    //     sort(nums.begin(), nums.end());
+    //     int prev = nums[0];
+    //     int maxLength = 1;
+    //     int length = 1;
+    
+    //     for(int i =1 ; i< nums.size(); i++){
+    //         if(prev == nums[i]){
+    //             continue;
+    //         }
+    //        if(prev + 1 == nums[i]){
+    //          prev++;
+    //          length ++;
+    //         }
+    //         else {
+    //           prev = nums[i];
+    //           length = 1;
+    //          }
+    //      maxLength = max(length, maxLength);
+    //     }
+    //     return maxLength;
+    // }
 };
