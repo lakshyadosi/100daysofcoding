d0e72fa1512e1afb697d929981aa34476aa09cdc
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-07-12 16:18:01 +0530

    Time: 31 ms (16.39%), Space: 23 MB (28.17%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end()); // most important thing
         int n = intervals.size();
-    vector<vector<int>> ans;
+         vector<vector<int>> ans;
         for(int i=0; i< n;i++){
 
             // for i=0, there is no comparison thats why also when there is only single array
