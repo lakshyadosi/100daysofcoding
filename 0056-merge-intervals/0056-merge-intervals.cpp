80d4f83c8d8aa225e7755bec3c9c4e458f0d7d8e
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-07-12 16:16:49 +0530

    Time: 19 ms (85.44%), Space: 23 MB (27.64%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
+        sort(intervals.begin(), intervals.end()); // most important thing
         int n = intervals.size();
     vector<vector<int>> ans;
         for(int i=0; i< n;i++){
+
+            // for i=0, there is no comparison thats why also when there is only single array
             if(ans.empty()){
                 ans.push_back(intervals[i]);
             }
@@ -16,6 +18,8 @@ public:
              if(intervals[i][0] <= endingValue){
                 v[1] = max(endingValue, intervals[i][1]);
              }
+             
+             //Non-overlapping case
              else{
                 ans.push_back(intervals[i]);
              }
