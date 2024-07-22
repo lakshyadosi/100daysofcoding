99547834e87302b8e08472276b85c862e9ef09bf
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-07-22 17:14:43 +0530

    Time: 5 ms (51.75%), Space: 16.8 MB (14.1%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -9,7 +9,7 @@ public:
             if(nums[mid] == target){
                 return true;
             }
-            if(nums[low] == nums[mid] == nums[high]){
+            if(nums[low] == nums[mid] && nums[mid]== nums[high]){
                 low++;
                 high--;
                 continue;
