70df1cfe21f72fb0b85e47e3fe38bced1cfc5984
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-08-01 22:08:44 +0530

    Time: 136 ms (17.95%), Space: 75.1 MB (35.79%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,34 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int i = 0, j = 0;
-        int size = fruits.size();
-        int maxi = 0; // Initialize maxi to 0
-        int k = 2;
-        unordered_map<int, int> myMap; // Use int as key type
-
-        while (j < size) {
-            myMap[fruits[j]]++;
-
-            while (myMap.size() > k) {
-                myMap[fruits[i]]--;
-                if (myMap[fruits[i]] == 0) {
-                    myMap.erase(fruits[i]);
-                }
-                i++;
-            }
-
-            int subArrayLength = j - i + 1;
-            maxi = max(maxi, subArrayLength);
-            j++;
-        }
-        return maxi;
+      int i =0;
+      int j = 0;
+      int size = fruits.size();
+      int maxi = -1;
+      int k=2;
+      unordered_map<int, int> myMap;
+      while(j < size){
+          myMap[fruits[j]] = myMap[fruits[j]] +1;
+          int mapLength = myMap.size();
+          if(mapLength <= k){
+              int subArrayLength = j-i+1;
+              maxi = max(maxi, subArrayLength);
+              j++;
+          }
+        //   else if(mapLength == k){
+        //       int subArrayLength = j-i+1;
+        //       maxi = max(maxi, subArrayLength);
+        //       j++;
+        //   }
+          else if(mapLength > k){
+              while(mapLength > k){ 
+                  myMap[fruits[i]] = myMap[fruits[i]] -1;
+                  if(myMap[fruits[i]] == 0){
+                      myMap.erase(fruits[i]);
+                       mapLength = myMap.size();
+                  }
+                  i++;
+                 
+              }
+              j++;
+              
+          }
+      }
+      return maxi;
     }
 };
