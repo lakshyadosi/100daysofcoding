c7721f683fb09bf3208b933142a1165923b762ff
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-09-12 22:43:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,66 +1,32 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m =matrix.size();
-        int n = matrix[0].size();
-        int x =1;
-        int y =1;
-        for(int j=0;j<n;j++)
-        {
-            if(matrix[0][j]==0)x=0;
-            
-        }
+        int n =matrix.size();
+        int m = matrix[0].size();
+
+        vector<int>x, y;
         
-        for(int i=0;i<m;i++)
-        {
-            if(matrix[i][0]==0)y=0;
-        }
-       
-            
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+        for(int i=0; i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j] == 0){
+                    x.push_back(i);
+                    y.push_back(j)
                 }
             }
         }
-        
-        
-         for(int j=1;j<n;j++)
-        {
-           if(matrix[0][j]==0)
-           {
-               for(int i=1;i<m;i++)
-                   matrix[i][j]=0;
-           }
-        }
-        
-        for(int i=1;i<m;i++)
-        {
-            if(matrix[i][0]==0)
-            {
-                for(int j=0;j<n;j++)
-                    matrix[i][j]=0;
+
+        for(int i=0 ; i<=x.size(); i++){
+            int index = x[i];
+            for(int j=0; j<m; j++){
+                matrix[index][j] = 0;
             }
         }
-        
-        if(y==0)
-        {
-            for(int i=0;i<m;i++)
-            {
-                matrix[i][0]=0;
+
+          for(int i=0 ; i<=y.size(); i++){
+            int index = y[i];
+            for(int j=0; j<n; j++){
+                matrix[j][index] = 0;
             }
-        }
-        
-         if(x==0)
-        {      
-             for(int j=0;j<n;j++)
-            matrix[0][j]=0;
-        
         }
     }
 };
