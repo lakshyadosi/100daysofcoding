ce5e9b484fd067a28775589893f7e2e7884d34b0
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-09-16 21:39:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.cpp
+++ Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.cpp
@@ -10,7 +10,7 @@ class Solution
     void transpose(vector<vector<int> >& matrix, int n)
     { 
         for(int i=0; i< n; i++){
-            for(int j=0; j<=i ; j++){
+            for(int j=0; j<i ; j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
