0b77d08fdf2092c09ec2ef42eee7588173b959dc
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-11-10 17:37:46 +0530

    Time: 4 ms (21.68%), Space: 61.8 MB (32.38%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,17 +1,42 @@
+
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int size = height.size();
+//         int maxArea = INT_MIN;
+//         for(int i=0; i< size; i++){
+//             for(int j=i+1; j < size; j++){
+//                 int minDiff = min(height[j], height[i]);
+//                 int area = (j-i) * minDiff;
+//                 maxArea = max(area, maxArea);
+//             }
+//         }
+//         return maxArea;
+//   }
+// };
+
+
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int size = height.size();
-        int maxArea = INT_MIN;
-        for(int i=0; i< size; i++){
-            int minDiff = INT_MAX;
-            for(int j=i+1; j < size; j++){
-                minDiff = min(height[j], height[i]);
-                int area = (j-i) * minDiff;
-                maxArea = max(area, maxArea);
-            }
-        }
-        return maxArea;
+       int size = height.size();
+       int left = 0;
+       int right = size -1;
+       int maxArea = INT_MIN;
+       while(left < right){
+          int width = right - left;
+          int minHeight = min(height[left], height[right]);
+          int area = width * minHeight;
+          maxArea = max(maxArea, area);
+          if(height[left] < height[right]){
+             left++;
+          }
+          else{
+            right--;
+          }
+
+       }
+       return maxArea;
   }
-    
 };
