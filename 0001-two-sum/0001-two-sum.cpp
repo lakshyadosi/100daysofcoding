45d03089a8a024e1a77633f373348b9f662612ab
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-11-10 10:35:02 +0530

    Time: 4 ms (65.58%), Space: 14.4 MB (36.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,44 @@
+/*
+Brute force approach
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
       int size = nums.size();
-      vector<int> result;
-      map<int, int> myMap;
-
-      for(int i=0; i< size; i++){
-        if(myMap.find(target-nums[i]) != myMap.end()){
-            result.push_back(myMap[target - nums[i]]);
-            result.push_back(i);
-        }
-        else{
-            myMap[nums[i]] = i;
-        }
+      for(int i=0; i < size; i++){
+          for(int j=i+1;j < size;j++){
+            if(nums[i] + nums[j] == target){
+                return {i,j};
+            } 
+          }
       }
-      return result;
+      return {-1,-1};
+    }
+};
+
+*/
+
+// Hashmap based approach
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int targetSum) {
+        int arraySize = numbers.size();
+        vector<int> indices;  
+        unordered_map<int, int> numberToIndexMap;
+
+        for (int currentIndex = 0; currentIndex < arraySize; currentIndex++) {
+            int requiredNumber = targetSum - numbers[currentIndex];
+
+            // Check if the required number already exists in the map
+            if (numberToIndexMap.find(requiredNumber) != numberToIndexMap.end()) {
+                indices.push_back(numberToIndexMap[requiredNumber]); 
+                indices.push_back(currentIndex); 
+                return indices;  
+            }
+
+            // If not found, add the current number and its index to the map
+            numberToIndexMap[numbers[currentIndex]] = currentIndex;
+        }
+
+        return {-1,-1}; 
     }
 };
