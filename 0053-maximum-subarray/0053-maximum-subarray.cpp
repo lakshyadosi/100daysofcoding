e533f9d9ed1c4328feedc309f9b7501dcfd08e3f
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-11-10 11:44:16 +0530

    Time: 0 ms (100%), Space: 70.6 MB (22.22%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,20 +1,38 @@
+/*
 class Solution {
 public:
     int maxSubArray(vector<int>& arr) {
-        int sum = 0;
-        int maxSum = INT_MIN;
-        
-        for(int i=0; i< arr.size(); i++){
-            if(sum >=0){
-                sum = sum + arr[i];
-            }
-            else{
-                sum = arr[i];
-            }
-            if(sum > maxSum){
-                maxSum = sum;
-            }
+       int maxSum = INT_MIN;
+       int n = arr.size();
+
+       for(int i=0; i<n;i++){
+        int currSum = 0;
+        for(int j=i; j< n;j++){
+            currSum = currSum + arr[j];
+            maxSum = max(currSum, maxSum);
         }
-        return maxSum;  
+       }
+       return maxSum;
     }
 };
+*/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& arr) {
+       int maxSum = INT_MIN;
+       int n = arr.size();
+       int currSum = 0;
+
+       for(int i=0; i< n; i++){
+         currSum = currSum + arr[i];
+         maxSum = max(currSum, maxSum); // Update maxSum before resetting currSum to handle cases where all numbers are negative
+
+         if(currSum < 0){
+            currSum = 0;
+         }
+       }
+       return maxSum;
+    }
+};
+
