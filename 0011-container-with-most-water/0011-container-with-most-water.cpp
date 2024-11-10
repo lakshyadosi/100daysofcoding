0ba5a2006337855b4d6321333b20d7ab3303cc53
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-11-10 17:37:54 +0530

    Time: 2 ms (39.65%), Space: 61.8 MB (32.38%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,21 +1,31 @@
+/*
 
-// class Solution {
-// public:
-//     int maxArea(vector<int>& height) {
-//         int size = height.size();
-//         int maxArea = INT_MIN;
-//         for(int i=0; i< size; i++){
-//             for(int j=i+1; j < size; j++){
-//                 int minDiff = min(height[j], height[i]);
-//                 int area = (j-i) * minDiff;
-//                 maxArea = max(area, maxArea);
-//             }
-//         }
-//         return maxArea;
-//   }
-// };
+Brute force approach
 
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int size = height.size();
+        int maxArea = INT_MIN;
+        for(int i=0; i< size; i++){
+            for(int j=i+1; j < size; j++){
+                int minDiff = min(height[j], height[i]);
+                int area = (j-i) * minDiff;
+                maxArea = max(area, maxArea);
+            }
+        }
+        return maxArea;
+  }
+};
 
+*/
+
+/*
+The area is calculated as ( \text{area} = \text{width} \times \text{height} ). 
+If we reduce the width, we need to ensure that we have the maximum possible height to maintain or increase the area.
+Thats why if left is minimum then we move left pointer
+
+*/
 
 class Solution {
 public:
