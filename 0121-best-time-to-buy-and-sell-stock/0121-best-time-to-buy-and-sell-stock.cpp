0986119d187d084de816bfe85f4614db706345fd
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-11-10 10:45:59 +0530

    Time: 1 ms (38.4%), Space: 96 MB (98.7%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,21 @@
+/*
+Time Complexity: \U0001d442(\U0001d45b)
+Space Complexity: O(1)
+
+*/
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min_price_to_sell = prices[0];
+        int min_price_to_buy = prices[0];
         int max_profit = 0;
 
-        for(int i=1; i< prices.size(); i++){
-            if(prices[i] < min_price_to_sell){
-                min_price_to_sell = prices[i];
+        for(int currentIndex=1; currentIndex< prices.size(); currentIndex++){
+            if(prices[currentIndex] < min_price_to_buy){
+                min_price_to_buy = prices[currentIndex];
             }
             else{
-                max_profit = max(max_profit, prices[i] - min_price_to_sell);
+                max_profit = max(max_profit, prices[currentIndex] - min_price_to_buy);
             }
         } 
         return max_profit;
