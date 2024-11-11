3a68e8aa3e0751c0474c918e891ccb5d851d05dd
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2024-11-11 10:33:56 +0530

    Time: 68 ms (15.65%), Space: 75.1 MB (66.55%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-      int i =0;
-      int j = 0;
-      int size = fruits.size();
-      int maxi = -1;
-      int k=2;
-      unordered_map<int, int> myMap;
-      while(j < size){
-          myMap[fruits[j]] = myMap[fruits[j]] +1;
-          int mapLength = myMap.size();
-          if(mapLength <= k){
-              int subArrayLength = j-i+1;
-              maxi = max(maxi, subArrayLength);
-              j++;
-          }
-        //   else if(mapLength == k){
-        //       int subArrayLength = j-i+1;
-        //       maxi = max(maxi, subArrayLength);
-        //       j++;
-        //   }
-          else if(mapLength > k){
-              while(mapLength > k){ 
-                  myMap[fruits[i]] = myMap[fruits[i]] -1;
-                  if(myMap[fruits[i]] == 0){
-                      myMap.erase(fruits[i]);
-                       mapLength = myMap.size();
-                  }
-                  i++;
-                 
+       int left = 0;
+       int right = 0;
+       int maxFruits = INT_MIN;
+       int size = fruits.size();
+       unordered_map<int, int> numerToCountMap;
+
+       while(right < size){
+           numerToCountMap[fruits[right]] = numerToCountMap[fruits[right]] + 1;
+           
+           while(numerToCountMap.size() > 2){
+              numerToCountMap[fruits[left]] =  numerToCountMap[fruits[left]] -1;
+              if( numerToCountMap[fruits[left]] == 0){
+                numerToCountMap.erase(fruits[left]);
               }
-              j++;
-              
-          }
-      }
-      return maxi;
+              left++;
+           }
+  
+           maxFruits = max(maxFruits, right-left+1);
+           right++;
+       }
+       return maxFruits;
     }
 };
