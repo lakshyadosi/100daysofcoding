f3653eee21b747a6c966f76bffef7cce467312dc
Lakshya dosi <50392409+lakshyadosi@users.noreply.github.com>
2025-05-29 17:24:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+   let myMap = new Map();
+   
+   for(let ch of s){
+      myMap.set(ch, (myMap.get(ch) || 0) + 1)
+   }
+
+   for(let ch of t){
+     if(myMap.has(ch)){
+        myMap.set(ch, myMap.get(ch) -1);
+        if(myMap.get(ch) == 0){
+            myMap.delete(ch);
+        }
+     }
+   }
+
+   if(myMap.size == 0){
+      return true;
+   }
+   else{
+    return false;
+   }
+};
